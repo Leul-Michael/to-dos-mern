2c0aefab7f0c141bae83f4563dac3fac25706a2f
leu_l <leulmic18@gmail.com>
2022-09-26 17:27:12 +0300

    cors update

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,49 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const { errorMiddleware } = require("./middleware/errorMiddleware");
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { errorMiddleware } = require("./middleware/errorMiddleware")
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 
 // Conntect  to Database
 mongoose.connect(
   process.env.MONGO_URL,
   () => {
-    console.log("Connected to DB...");
+    console.log("Connected to DB...")
   },
   (e) => {
-    console.error(e);
+    console.error(e)
   }
-);
+)
 
 // EXPRESS JSON HANDLER
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 
-app.use("/api/users", require("./routes/users"));
-app.use("/api/todos", require("./routes/todos"));
-app.use("/api/lists", require("./routes/lists"));
+app.use("/api/users", require("./routes/users"))
+app.use("/api/todos", require("./routes/todos"))
+app.use("/api/lists", require("./routes/lists"))
 
 // Frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
 
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
-  );
+  )
 } else {
-  app.get("/", (req, res) => res.send("Please set to production"));
+  app.get("/", (req, res) => res.send("Please set to production"))
 }
 
-app.use(errorMiddleware);
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
-  console.log("Server started on port " + PORT + "...");
-});
+  console.log("Server started on port " + PORT + "...")
+})
